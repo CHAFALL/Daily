9fc6a6e80eac5453f1dd7bac5e8154bcd5d05e93
전근렬 <hm04137@naver.com>
2023-10-16 17:55:55 +0900

    1016

--- 1. study/pythonProject/study/14503(로봇 청소기).py
+++ 1. study/pythonProject/study/14503(로봇 청소기).py
@@ -1,29 +1,24 @@
-import sys
-sys.setrecursionlimit(10000)
-
-def dfs(r, c, d, cnt, total):
-    visited[r][c] = 1  # 방문체크
-
-    if cnt == 4:
-        # 뒤로 돌기
-        d = (d-2) % 4
-        nr = r + dr[d]
-        nc = c + dc[d]
-        if arr[nr][nc] == 1 or visited[nr][nc]:
+def dfs(r, c, d):
+    total = 0
+    cnt = 0
+    while cnt < 3:
+        nr = r + dr[d - 1]
+        nc = c + dc[d - 1]
+        if arr[nr][nc] == 0:
+            total += 1
+            arr[nr][nc] = 2
+            dfs(nr, nc, d)
+    else:
+        # 후진용(뒤로 돌아)
+        nd = d - 2
+        nr = r + dr[nd]
+        nc = c + dc[nd]
+        if arr[nr][nc] == 1:
             print(total)
             return
-        else:
-            dfs(nr, nc, d, 0, total)
 
-    while cnt < 4:
-        d = (d-1) % 4
-        nr = r + dr[d]
-        nc = c + dc[d]
-        
-        if arr[nr][nc] == 0 and visited[nr][nc] == 0:
-            dfs(nr, nc, d, 0, total + 1)
-            break
-        cnt += 1
+
+
 
 # 북, 동, 남, 서
 dr = [-1, 0, 1, 0]
@@ -31,7 +26,5 @@ dc = [0, 1, 0, -1]
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
-dfs(r, c, d, 0, 1)
-#----------------------------------------------------------
-# 머리 돌아갈 때 다시 풀기
+# visited = [[0] * M for _ in range(N)]
+dfs(r, c, d)
