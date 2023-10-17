481ee6eca6142c32c25155367da081fc1d3eedaa
전근렬 <hm04137@naver.com>
2023-10-17 17:53:35 +0900

    1017

--- 1. study/pythonProject/study/14503(로봇 청소기).py
+++ 1. study/pythonProject/study/14503(로봇 청소기).py
@@ -1,5 +1,5 @@
 def dfs(r, c, d):
-    total = 0
+    global total
     cnt = 0
     while cnt < 3:
         nr = r + dr[d - 1]
@@ -8,6 +8,7 @@ def dfs(r, c, d):
             total += 1
             arr[nr][nc] = 2
             dfs(nr, nc, d)
+        cnt += 1
     else:
         # 후진용(뒤로 돌아)
         nd = d - 2
@@ -16,8 +17,8 @@ def dfs(r, c, d):
         if arr[nr][nc] == 1:
             print(total)
             return
-
-
+        else:
+            dfs(nr, nc, d)
 
 
 # 북, 동, 남, 서
@@ -27,4 +28,5 @@ N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 # visited = [[0] * M for _ in range(N)]
+total = 0
 dfs(r, c, d)
---
+++ 1. study/pythonProject/study/1931(회의실 배정).py
@@ -0,0 +1,3 @@
+N = int(input())
+for _ in range(N):
+    s, e = map(int, input().split())
