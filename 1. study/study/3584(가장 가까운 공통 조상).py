d8438f52ee93391af807a2db40eb3084e96cd80f
전근렬 <hm04137@naver.com>
2023-10-27 17:50:50 +0900

    1027

---
+++ 1. study/study/3584(가장 가까운 공통 조상).py
@@ -0,0 +1,11 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+
+    tree = [[0] * 3 for _ in range(N + 1)]
+    for _ in range(N - 1):
+
+
+
+    # 가장 가까운 공통 조상을 구할 두 노드
+    A, B = map(int, input().split())
