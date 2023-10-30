da2a283dbf26caa5b3d1a65e1bab251cf5df6a25
전근렬 <hm04137@naver.com>
2023-10-30 18:00:57 +0900

    1030

---
+++ 1. study/Django/Django_practice/수정본/movies/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from .models import Actor, Movie, Review
+
+class ActorListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class MovieTitleSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('title',)
+
+    movies = MovieTitleSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('title', 'overview',)
+
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('title', 'content',)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    
+    actors = ActorListSerializer(many=True, read_only=True)
+    
+    review_set = ReviewListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    
+    class MovieTitleSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('title',)
+    
+    movie = MovieTitleSerializer(read_only=True)
+    
+    class Meta:
+        model = Review
+        fields = '__all__'
+        # read_only_fields = ('movie',)
