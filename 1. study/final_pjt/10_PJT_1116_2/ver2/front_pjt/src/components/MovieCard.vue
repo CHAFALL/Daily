7fdcb8a9fe29dcfa03054f2963b982df8ea33fdb
전근렬 <hm04137@naver.com>
2023-11-16 22:05:23 +0900

    1116

---
+++ 1. study/final_pjt/10_PJT_1116_2/ver2/front_pjt/src/components/MovieCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @click="goDetail">
+    <h5>{{ movie.id }}</h5>
+    <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="#">
+    <hr>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+const props = defineProps({
+  movie:Object,
+})
+
+const goDetail = () => {
+  router.push(`movies/${props.movie.id}`)
+}
+</script>
+<style scoped>
+
+</style>
---
+++ 1. study/final_pjt/10_PJT_1116_2/ver2/front_pjt/src/views/MovieView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Movie Page</h1>
+    <RouterLink :to="{name:'CreateView'}">[CREATE]
+    </RouterLink>
+    <MovieCard 
+    v-for="movie in store.movies" 
+    :key="movie.id" 
+    :movie="movie" 
+    />
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useCounterStore } from '@/stores/counter';
+import { RouterLink } from 'vue-router'
+import MovieCard from '@/components/MovieCard.vue';
+const store = useCounterStore()
+
+onMounted(()=>{
+  store.getMovies()
+})
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
