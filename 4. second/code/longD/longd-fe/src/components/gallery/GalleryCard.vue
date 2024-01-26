82fb3fc06b525c621c4b946a9e7f432af0d5ee8b
전근렬 <hm04137@naver.com>
2024-01-26 17:56:20 +0900

    0126

---
+++ 4. second/code/longD/longd-fe/src/components/gallery/GalleryCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card card-compact w-96 bg-base-100 shadow-xl">
+    <figure>
+      <img :src="src" alt="#" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ id }}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  id: {
+    type: Number,
+  },
+  src: {
+    type: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
