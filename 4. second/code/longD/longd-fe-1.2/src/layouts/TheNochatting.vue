8495d94509e1a62cd0fceeba6fddf4986e70922f
전근렬 <hm04137@naver.com>
2024-02-14 23:49:29 +0900

    0214

---
+++ 4. second/code/longD/longd-fe-1.2/src/layouts/TheNochatting.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="h-[45rem] flex flex-col items-center justify-center border border-black"
+  >
+    <button @click="turnChat">
+      <img
+        src="/static/img/message-regular.svg"
+        alt="asas"
+        style="width: 30px; height: 30px; margin: 0 5px"
+      />
+    </button>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['onChat']);
+const turnChat = function () {
+  emit('onChat');
+};
+</script>
+
+<style scoped></style>
