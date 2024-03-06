5a3b1e4abeb0ac438eeba8366897fdaa7f241171
전근렬 <hm04137@naver.com>
2024-03-06 18:00:25 +0900

    0306

---
+++ 4. second/code/dev/Front/taiso/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainPage(props){
+  return (
+    <div className={'text-3xl'}>
+      <div>Main Page</div>
+    </div>
+  );
+}
+
+export default MainPage;
+
+
---
+++ 4. second/code/dev/Front/taiso/src/router/root.js
@@ -0,0 +1,13 @@
+import MainPage from "../pages/MainPage";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+
+const root = createBrowserRouter([
+  {
+    path: '',
+    element: <MainPage />
+  }
+])
+
+export default root
