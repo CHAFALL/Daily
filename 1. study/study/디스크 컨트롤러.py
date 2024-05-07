7c7508c912d14f64a81e1a74737e031b2d2ea89e
전근렬 <hm04137@naver.com>
2024-05-07 21:55:10 +0900

    0507

--- 1. study/study/디스크 컨트롤러.py
+++ 1. study/study/디스크 컨트롤러.py
@@ -2,7 +2,7 @@ import heapq
 
 def solution(jobs):
     answer = cnt = now = 0
-    prev_start = -1
+    prev = -1
 
     N = len(jobs)
     Q = [] # 요청시간, 걸리는 시간
@@ -11,11 +11,11 @@ def solution(jobs):
         # 힙에 넣을 수 있는 조건:
         # 이전 것의 시작시각보다 크고 현재시각보다 시작시각이 작거나 같을 때
         for job in jobs:
-            if prev_start < job[0] <= now:
+            if prev < job[0] <= now:
                 heapq.heappush(Q, (job[1], job[0])) # 걸리는 시간이 앞으로 가도록
         if Q:
             time, start_time = heapq.heappop(Q)
-            prev_start = start_time
+            prev = now # -> 이걸하는 이유 : 전에 했던 것이 또 들어가지 않게 하려고
             now += time # 걸린 시간만큼 현재시간 계산
             answer += now - start_time
             cnt += 1
